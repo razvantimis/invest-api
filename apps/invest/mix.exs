1b26b7794ce3f073025dec71d07117489a6978fe
Razvan Timis <r@timis.dev>
2022-06-21 17:30:37 +0300

    Add back

--- apps/invest/mix.exs
+++ apps/invest/mix.exs
@@ -51,7 +51,7 @@ defmodule Invest.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get"],
+      setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
