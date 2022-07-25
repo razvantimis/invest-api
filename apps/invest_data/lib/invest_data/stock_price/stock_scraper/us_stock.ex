9ad23e3548eff8f30558a2456c38bb0ebc769f78
Razvan Timis <r@timis.dev>
2022-07-25 12:05:37 +0300

    Catch error

--- apps/invest_data/lib/invest_data/stock_price/stock_scraper/us_stock.ex
+++ apps/invest_data/lib/invest_data/stock_price/stock_scraper/us_stock.ex
@@ -6,21 +6,33 @@ defmodule InvestData.StockScraper.UsStock do
     InvestData.UsStockScraper.fetch_stock_price("MMM")
   """
   def fetch_stock_price(symbol) do
-    case YahooFinance.Quote.get_simple_quote(symbol) do
-      {:error, _} ->
-        Logger.error("[fetch_stock_price] Error  #{symbol}...")
+    try do
+      case YahooFinance.Quote.get_simple_quote(symbol) do
+        {:error, _} ->
+          Logger.error("[fetch_stock_price] Error  #{symbol}...")
+          {:error, []}
+
+        {:ok, {_symbol, result}} ->
+          results = Jason.decode!(result) |> Map.get("quoteResponse") |> Map.get("result")
+          cond do
+            Enum.count(results) > 0 ->
+              [result | _] = results
+
+              stock_price = %{
+                date: Date.utc_today(),
+                price: result |> Map.get("regularMarketPrice")
+              }
+
+              {:ok, [stock_price]}
+
+            Enum.empty?(results) ->
+              {:error, []}
+          end
+      end
+    rescue
+      e ->
+        Logger.error(Exception.format(:error, e, __STACKTRACE__))
         {:error, []}
-
-      {:ok, {_symbol, result}} ->
-        [result | _] = Jason.decode!(result) |> Map.get("quoteResponse") |> Map.get("result")
-
-        stock_price = %{
-          date: Date.utc_today(),
-          price: result |> Map.get("regularMarketPrice")
-        }
-
-        {:ok, [stock_price]}
     end
   end
-
 end
